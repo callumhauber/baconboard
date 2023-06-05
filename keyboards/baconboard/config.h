45185c85c9df89e58c6346f2b61afd99cb7643ac
Callum Hauber <callumhauber@gmail.com>
2023-06-04 22:37:00 -0400

    initial commit

---
+++ keyboards/baconboard/config.h
@@ -0,0 +1,27 @@
+// Copyright 2023 Bacon (@callumhauber)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define USB_VBUS_PIN GP9
+
+/*
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+*/
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
